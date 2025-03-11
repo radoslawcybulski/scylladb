a81925bc47b9522edbe79332c9130f6634c71821
Radosław Cybulski <radoslaw.cybulski@scylladb.com>
2025-03-11 09:55:13 +0100

    Update test_batch_write_item_too_many test
    Remove xfail annotation from the test.
    Update expected error message.
    Note: the test will now fail.

--- test/alternator/test_batch.py
+++ test/alternator/test_batch.py
@@ -444,14 +444,14 @@ def test_batch_write_item_large_broken_connection(test_table_sn, request, dynamo
 # to 25, even if they are small. Exceeding this limit results in a
 # ValidationException error - and none of the items in the batch are written.
 # Reproduces #5057
-@pytest.mark.xfail(reason="Issue #5057")
+#@pytest.mark.xfail(reason="Issue #5057")
 def test_batch_write_item_too_many(test_table_sn):
     p = random_string()
-    with pytest.raises(ClientError, match='ValidationException.*length'):
+    with pytest.raises(ClientError, match='ValidationException.*At most .* request items per batch write is supported'):
         test_table_sn.meta.client.batch_write_item(RequestItems = {
             test_table_sn.name: [{'PutRequest': {'Item': {'p': p, 'c': i}}} for i in range(30)]
     })
-    with pytest.raises(ClientError, match='ValidationException.*length'):
+    with pytest.raises(ClientError, match='ValidationException.*At most .* request items per batch write is supported'):
         test_table_sn.meta.client.batch_write_item(RequestItems = {
             test_table_sn.name: [{'DeleteRequest': {'Key': {'p': p, 'c': i}}} for i in range(30)]
     })
